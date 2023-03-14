0bc82b299737e3bcec7066f71c265454b8961b84
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-14 09:34:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,3 +19,5 @@ upload_file = st.file_uploader("Upload the Chest X-ray", type = 'jpg')
 
 # creating a predict button
 generate_pred = st.button("Predict")
+
+model = tf.keras.models.load_model('Covid_Classifier.h5', compile=False)
