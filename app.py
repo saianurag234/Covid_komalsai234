912f33b4d163c17b44a614133d798e502932ab80
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-14 21:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ st.sidebar.subheader("Udayagiri Varun")
 st.sidebar.subheader("Sejal Singh")
 st.sidebar.image("https://post.healthline.com/wp-content/uploads/2020/08/chest-x-ray_thumb.jpg", width=None)
 
-# creating a predict button
-generate_pred = st.button("Predict")
-
 # creating an uploader to upload the Chest X-ray images
 uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
+
+# creating a predict button
+generate_pred = st.button("Predict")
 
 if generate_pred:
     if uploaded_file is not None:
